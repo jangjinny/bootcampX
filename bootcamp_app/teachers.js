d7c117c982806f53f3c2c9d6daf054990df8610b
Jinny Jang <jangjinny21@gmail.com>
2021-02-04 19:35:56 +0000

    create more queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,7 +1,6 @@
 const { Pool } = require('pg');
-const args = process.argv[2];
 
-const pool = new Pool({
+const pool = new Pool ({
   user: 'vagrant',
   password: '123',
   host: 'localhost',
@@ -9,14 +8,16 @@ const pool = new Pool({
 });
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
+SELECT teachers.name as name, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
+ORDER BY name;
 `)
 .then(res => {
   res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+    console.log(`${user.cohort}: ${user.name}`);
   })
 }).catch(err => console.error('query error', err.stack));
