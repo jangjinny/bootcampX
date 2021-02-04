4ae1c24102367889867dd5eac13d00d8ca1b0bef
Jinny Jang <jangjinny21@gmail.com>
2021-02-04 19:48:17 +0000

    include parametereized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,5 +1,9 @@
 const { Pool } = require('pg');
 
+const cohortName = process.argv[2];
+// Store all potentially malicious values in an array. 
+const values = [`%${cohortName}%`];
+
 const pool = new Pool ({
   user: 'vagrant',
   password: '123',
@@ -7,15 +11,17 @@ const pool = new Pool ({
   database: 'bootcampx'
 });
 
-pool.query(`
+const queryString = `
 SELECT teachers.name as name, cohorts.name as cohort
 FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE $1
 ORDER BY name;
-`)
+`
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.cohort}: ${user.name}`);
